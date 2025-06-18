6c5d092d5bbbb46309064f8f3a3a5d4a8b7ebaa0
Sahil Gagan <51903665+sahil2k19@users.noreply.github.com>
2025-06-18 11:42:57 +0530

    first commit

---
+++ DynamicProgramming/newtest.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long int ll;
+typedef long double ld;
+
+void solve(){
+    
+}
+
+int main(){
+    int t;
+    t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    
+}
